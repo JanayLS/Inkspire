c4618afbb9bad52d99b1743536d0dd5aa4955b11
Janay S <jlsnell@my.waketech.edu>
2026-05-04 20:38:34 -0400

    Ready for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn inkspire_project.wsgi
--- inkspire_project/settings.py
+++ inkspire_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-%17r-zj)2&&#yj_1e6wn-c-y^xkxe=lnz0-i22#0d12%29q_0a"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['your-app-name.onrender.com']
 
 
 # Application definition
@@ -128,3 +128,4 @@ MEDIA_ROOT = "media"
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'  
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
