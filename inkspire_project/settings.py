a1f949bbe5886cbfeacb3a29c742a66775fcad9a
Janay S <jlsnell@my.waketech.edu>
2026-05-06 19:01:34 -0400

    Fix duplicate cloudinary app

--- inkspire_project/settings.py
+++ inkspire_project/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "gallery",
     "cloudinary",
     "cloudinary_storage",
-    "gallery",
 ]
 INSTALLED_APPS += ['cloudinary', 'cloudinary_storage']
 
