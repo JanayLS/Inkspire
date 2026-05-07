a440dccd03a399818b2042adce978a7b7929e704
Janay S <jlsnell@my.waketech.edu>
2026-05-06 21:48:21 -0400

    Enable debug temporarily

--- inkspire_project/settings.py
+++ inkspire_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-%17r-zj)2&&#yj_1e6wn-c-y^xkxe=lnz0-i22#0d12%29q_0a"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.onrender.com']
 
