929da9308a513f0671a1744fc64e302da6574bef
Janay S <jlsnell@my.waketech.edu>
2026-05-20 00:09:00 -0400

    Fix artwork admin owner display

--- gallery/admin.py
+++ gallery/admin.py
@@ -10,10 +10,17 @@ class ProfileAdmin(admin.ModelAdmin):
 
 @admin.register(Artwork)
 class ArtworkAdmin(admin.ModelAdmin):
-    list_display = ('title', 'owner', 'created_at')
-    list_filter = ('created_at', 'owner')
+    list_display = ['title', 'get_owner', 'created_at']
+    list_filter = ('created_at', 'gallery')
     search_fields = ('title', 'description')
     ordering = ('-created_at',)
+
+    def get_owner(self, obj):
+        if obj.gallery and obj.gallery.owner:
+            return obj.gallery.owner.username
+        return "No Owner"
+
+    get_owner.short_description = "Owner"
 
 
 @admin.register(Comment)
